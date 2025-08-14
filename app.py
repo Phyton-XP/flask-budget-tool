5d5be1db0ea8604de5f23d8ed193eb2cd030809e
Phyton-XP <226338965+Phyton-XP@users.noreply.github.com>
2025-08-14 10:10:29 +0200

    app.py
    Remove debug mode for release

--- app.py
+++ app.py
@@ -214,4 +214,4 @@ def clear_budgets():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
